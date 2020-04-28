2ab890c3f7640af86d3e4efa73aec6b598867ef9
TatsianaMark <tanya030890@mail.ru>
2020-04-28 13:15:58 +0200

    init:start example-site

---
+++ react-site/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <>
+      <h1>hi</h1>
+      <Button variant="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
