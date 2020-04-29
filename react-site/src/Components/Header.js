cca924854b5377544fb4837f654092e42fc49b77
TatsianaMark <tanya030890@mail.ru>
2020-04-29 18:13:45 +0200

    feat: add router-dom

--- react-site/src/Components/Header.js
+++ react-site/src/Components/Header.js
@@ -1,29 +1,55 @@
 import React, {Component} from 'react';
 import { Navbar, Nav, Container} from "react-bootstrap";
+import { BrowserRouter as Router, Switch,Route,Link } from "react-router-dom";
 
-export default class Header extends Component {
-    render() {
-        return (
-            <>
-               <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
-                   <Container>
-                       <Navbar.Brand href="/" >
-                             <div>Photographer Katrin</div>
-                       </Navbar.Brand>
-                       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                       <Navbar.Collapse id="responsive-navbar-nav">
-                           <Nav className="ml-auto">
-                               <Nav.Link href="/"> Главная </Nav.Link>
-                               <Nav.Link href="/about"> Обо мне </Nav.Link>
-                               <Nav.Link href="/services"> Услуги </Nav.Link>
-                               <Nav.Link href="/portfolio"> Портфолио </Nav.Link>
-                               <Nav.Link href="/contacts"> Контакты </Nav.Link>
-                           </Nav>
-                       </Navbar.Collapse>
-                       </Container>
-               </Navbar>
-            </>
-        );
-    }
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Services from "../Pages/Services";
+import Portfolio from "../Pages/Portfolio";
+import Contacts from "../Pages/Contacts";
+
+
+
+
+export default function Header() {
+    return (
+        <>
+            <Router>
+                <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="/" >
+                            <div className='logo'>Photographer Katrin</div>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="ml-auto">
+                                <Nav.Link as={Link} to='/'>Главная</Nav.Link>
+                                <Nav.Link as={Link} to="/about"> Обо мне </Nav.Link>
+                                <Nav.Link as={Link} to="/services"> Услуги </Nav.Link>
+                                <Nav.Link as={Link} to="/portfolio"> Портфолио </Nav.Link>
+                                <Nav.Link as={Link} to="/contacts"> Контакты </Nav.Link>
+
+
+
+                                {/*<Nav.Link href="/"> Главная </Nav.Link>*/}
+                                {/*<Nav.Link href="/about"> Обо мне </Nav.Link>*/}
+                                {/*<Nav.Link href="/services"> Услуги </Nav.Link>*/}
+                                {/*<Nav.Link href="/portfolio"> Портфолио </Nav.Link>*/}
+                                {/*<Nav.Link href="/contacts"> Контакты </Nav.Link>*/}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/about" component={About}/>
+                    <Route exact path="/services" component={Services}/>
+                    <Route exact path="/portfolio" component={Portfolio}/>
+                    <Route exact path="/contacts" component={Contacts}/>
+                    {/*<Route component={Default} />*/}
+                </Switch>
+            </Router>
+        </>
+    );
 }
-
