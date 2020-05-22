1f9c2e448de0bd933444f111858e23faf93afc37
TatsianaMark <tanya030890@mail.ru>
2020-05-22 17:10:23 +0200

    feat: regulate photo in portfolio

--- react-site/src/Pages/Portfolio.js
+++ react-site/src/Pages/Portfolio.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {Container, CardDeck, Card,Button , Row, Col, Nav, Tab} from "react-bootstrap";
-import horizont_first_s from '../assets/img/for_portfolio/see_more_second/horizont_first(7).jpg';
-import vertical_first from '../assets/img/for_portfolio/see_more_seventh/vertical_first (1).jpg';
-import winter_pho from '../assets/img/for_portfolio/see_more_fifth/winter_pho (2).jpg';
-import grey_ph from '../assets/img/for_portfolio/see_more_first/grey_ph (1).jpg';
+import horizont_first_s from '../assets/img/for_portfolio/see_more_second/horizont_first(3).jpg';
+import vertical_first from '../assets/img/for_portfolio/see_more_seventh/vertical_first (2).jpg';
+import winter_pho from '../assets/img/for_portfolio/see_more_fifth/winter_pho (1).jpg';
+import grey_ph from '../assets/img/for_portfolio/see_more_first/grey_ph (2).jpg';
 import loveImg from '../assets/img/for_portfolio/see_more_third/loveImg (2).jpg';
 import lightImg from '../assets/img/for_portfolio/see_more_sixth/lightImg (1).jpg';
 import white_pho from '../assets/img/for_portfolio/see_more_fourth/white_pho (2).jpg';
@@ -14,11 +14,11 @@ export default function Portfolio() {
             <h2 className="text-center m-4">Портфолио</h2>
             <CardDeck>
                 <div className="portfolio-cards d-flex">
-                        <Card>
-                            <Card.Img
-                                src={horizont_first_s}
-                            />
-                        </Card>
+                    <Card>
+                        <Card.Img
+                            src={grey_ph}
+                        />
+                    </Card>
                     <Card>
                         <Card.Img
                             src={winter_pho}
@@ -31,14 +31,15 @@ export default function Portfolio() {
                             />
                         </Card>
                     {/*----------------------------- sec-col----------------------------*/}
+
+                <Card>
+                    <Card.Img
+                        src={horizont_first_s}
+                    />
+                </Card>
                     <Card>
                         <Card.Img
-                            src={grey_ph}
-                        />
-                    </Card>
-                    <Card>
-                        <Card.Img
-                            src={white_pho}
+                           src={lightImg}
                         />
                     </Card>
                         <Card>
@@ -48,7 +49,7 @@ export default function Portfolio() {
                         </Card>
                     <Card>
                         <Card.Img
-                            src={lightImg}
+                            src={white_pho}
                         />
                     </Card>
                 {/*</div>*/}
