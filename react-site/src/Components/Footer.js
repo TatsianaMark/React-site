8d9625c890fcd05f00e5602ae966b8a22ef33988
TatsianaMark <tanya030890@mail.ru>
2020-05-23 20:15:02 +0200

    feat: add footer

--- react-site/src/Components/Footer.js
+++ react-site/src/Components/Footer.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Footer = () => {
     return(
-        <div className="container-footer">
-
-
-        </div>
+        <>
+            <div className="main-footer bg-dark px-5 py-3 text-white">
+                    Авторские права ©2020
+            </div>
+        </>
     )
 };
 
